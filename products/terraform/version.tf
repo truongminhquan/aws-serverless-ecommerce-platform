e25202fda56192516d21db14ac2af661bc0b4dda
truongminhquan <truongminhquan1999@gmail.com>
2023-08-27 15:10:01 +0700

    update

---
+++ products/terraform/version.tf
@@ -0,0 +1,14 @@
+terraform {
+required_version = ">= 0.12"
+  backend "s3" {
+    bucket         = "quantm15-terraform-serverless"
+    key            = "tfstates/products-svc.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "dev-serverless"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
