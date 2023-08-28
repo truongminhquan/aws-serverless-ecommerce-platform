4a97964774b488766ad3e15c17ff36560b6553f7
truongminhquan <truongminhquan1999@gmail.com>
2023-08-28 22:58:19 +0700

    update

--- products/terraform/main.tf
+++ products/terraform/main.tf
@@ -97,7 +97,7 @@ resource "aws_lambda_function" "product_validate_function" {
 }
 
 resource "aws_api_gateway_rest_api" "products-svc-api-gateway" {
-  body = "${jsondecode(file("../resources/openapi.json"))}"
+  body = "${file("../resources/openapi.json")}"
 
   name = "products-svc-api-gateway"
 
