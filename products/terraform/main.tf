4daea62ed204ae4f4a683ebbde353ef9f188ed0a
truongminhquan <truongminhquan1999@gmail.com>
2023-09-02 14:18:42 +0700

    update

--- products/terraform/main.tf
+++ products/terraform/main.tf
@@ -132,37 +132,3 @@ resource "aws_api_gateway_integration" "lambda" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.product_validate_function.invoke_arn
-  credentials             = aws_iam_role.lambda_exec.arn
-}
-
-resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_exec_policy_attach" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.product_validate_function.function_name
-  principal     = "apigateway.amazonaws.com"
-}
