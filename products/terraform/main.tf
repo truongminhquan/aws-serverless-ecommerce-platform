6bb852a7ce3908a68405cb1e648dac7941aa9cd5
truongminhquan <truongminhquan1999@gmail.com>
2023-09-02 13:17:46 +0700

    UPDATE

--- products/terraform/main.tf
+++ products/terraform/main.tf
@@ -105,3 +105,26 @@ resource "aws_api_gateway_rest_api" "products-svc-api-gateway" {
     types = ["REGIONAL"]
   }
 }
+
+resource "aws_api_gateway_resource" "products-svc-api-gateway-resource" {
+  rest_api_id = aws_api_gateway_rest_api.products-svc-api-gateway.id
+  parent_id   = aws_api_gateway_rest_api.products-svc-api-gateway.root_resource_id
+  path_part   = "/backend/validate"
+}
+
+resource "aws_api_gateway_method" "products-svc-api-gateway-resource-method" {
+  rest_api_id   = aws_api_gateway_rest_api.products-svc-api-gateway.id
+  resource_id   = aws_api_gateway_resource.products-svc-api-gateway-resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+# resource "aws_api_gateway_integration" "lambda" {
+#   rest_api_id = "${aws_api_gateway_rest_api.products-svc-api-gateway.id}"
+#   resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+#   http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = "${aws_lambda_function.example.invoke_arn}"
+# }
