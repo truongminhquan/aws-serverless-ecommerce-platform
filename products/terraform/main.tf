c83bfec461045ae868c76f18af3c9cc5ef539622
truongminhquan <truongminhquan1999@gmail.com>
2023-09-02 13:53:48 +0700

    update

--- products/terraform/main.tf
+++ products/terraform/main.tf
@@ -55,7 +55,7 @@ data "aws_iam_policy_document" "readpolicy" {
 
 resource "aws_iam_policy" "readpolicy" {
   name   = "Custom-DynamoDb-Read-Policy"
-  policy = "${data.aws_iam_policy_document.readpolicy.json}"
+  policy = data.aws_iam_policy_document.readpolicy.json
 }
 
 resource "aws_iam_role" "product_validate_function_iam_for_lambda" {
@@ -69,18 +69,18 @@ resource "aws_iam_role_policy_attachment" "lambda-role-readpolicy-attachment" {
 }
 
 data "archive_file" "simple_lambda_zip" {
-  source_dir = "../src/validate"
+  source_dir  = "../src/validate"
   output_path = "${path.module}/lambda_function_payload.zip"
-  type = "zip"
+  type        = "zip"
 }
 
 resource "aws_lambda_function" "product_validate_function" {
-  filename      =  data.archive_file.simple_lambda_zip.output_path
-  function_name = "product_validate_function"
+  filename         = data.archive_file.simple_lambda_zip.output_path
+  function_name    = "product_validate_function"
   source_code_hash = data.archive_file.simple_lambda_zip.output_base64sha256
-  role          = aws_iam_role.product_validate_function_iam_for_lambda.arn
-  handler       = "main.handler"
-  architectures = ["arm64"]
+  role             = aws_iam_role.product_validate_function_iam_for_lambda.arn
+  handler          = "main.handler"
+  architectures    = ["arm64"]
 
 
   runtime = "python3.9"
@@ -125,12 +125,11 @@ resource "aws_api_gateway_method" "products-svc-api-gateway-resource-method" {
   authorization = "NONE"
 }
 
-# resource "aws_api_gateway_integration" "lambda" {
-#   rest_api_id = "${aws_api_gateway_rest_api.products-svc-api-gateway.id}"
-#   resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-#   http_method = "${aws_api_gateway_method.proxy.http_method}"
-
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = "${aws_lambda_function.example.invoke_arn}"
-# }
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.products-svc-api-gateway.id
+  resource_id             = aws_api_gateway_method.products-svc-api-gateway-resource-method.resource_id
+  http_method             = aws_api_gateway_method.products-svc-api-gateway-resource-method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.product_validate_function.invoke_arn
+}
